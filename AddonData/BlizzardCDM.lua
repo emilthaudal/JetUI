a8a2b78b8e7451159111e188ed018183f1d169ef
Emil Thaudal Bønnerup <emil@thaudal.com>
2026-05-24 15:56:47 +0200

    Fix BlizzardCDM: rekey all specs to correct CDM tag scheme (11-133, not WoW spec IDs)

--- AddonData/BlizzardCDM.lua
+++ AddonData/BlizzardCDM.lua
@@ -4,56 +4,57 @@
 -- Replace PASTE_..._PROFILE_STRING_HERE with the real encoded string.
 
 JetUI.BlizzardCDMProfiles = {
-    -- Death Knight
-    [250] = { profileKey = "JetUI - Blood DK",       profileString = "PASTE_BLOOD_DK_PROFILE_STRING_HERE" },
-    [251] = { profileKey = "JetUI - Frost DK",       profileString = "PASTE_FROST_DK_PROFILE_STRING_HERE" },
-    [252] = { profileKey = "JetUI - Unholy DK",      profileString = "PASTE_UNHOLY_DK_PROFILE_STRING_HERE" },
-    -- Demon Hunter
-    [577] = { profileKey = "JetUI - Havoc DH",       profileString = "PASTE_HAVOC_DH_PROFILE_STRING_HERE" },
-    [581] = { profileKey = "JetUI - Vengeance DH",   profileString = "PASTE_VENGEANCE_DH_PROFILE_STRING_HERE" },
-    -- Druid
-    [102] = { profileKey = "JetUI - Balance Druid",  profileString = "PASTE_BALANCE_DRUID_PROFILE_STRING_HERE" },
-    [103] = { profileKey = "JetUI - Feral Druid",    profileString = "PASTE_FERAL_DRUID_PROFILE_STRING_HERE" },
-    [104] = { profileKey = "JetUI - Guardian Druid", profileString = "PASTE_GUARDIAN_DRUID_PROFILE_STRING_HERE" },
-    [105] = { profileKey = "JetUI - Resto Druid",    profileString = "PASTE_RESTO_DRUID_PROFILE_STRING_HERE" },
-    -- Evoker
-    [1467] = { profileKey = "JetUI - Devastation Evoker",   profileString = "PASTE_DEVASTATION_EVOKER_PROFILE_STRING_HERE" },
-    [1468] = { profileKey = "JetUI - Preservation Evoker",  profileString = "PASTE_PRESERVATION_EVOKER_PROFILE_STRING_HERE" },
-    [1473] = { profileKey = "JetUI - Augmentation Evoker",  profileString = "PASTE_AUGMENTATION_EVOKER_PROFILE_STRING_HERE" },
-    -- Hunter
-    [253] = { profileKey = "JetUI - BM Hunter",      profileString = "PASTE_BM_HUNTER_PROFILE_STRING_HERE" },
-    [254] = { profileKey = "JetUI - MM Hunter",      profileString = "PASTE_MM_HUNTER_PROFILE_STRING_HERE" },
-    [255] = { profileKey = "JetUI - SV Hunter",      profileString = "PASTE_SV_HUNTER_PROFILE_STRING_HERE" },
-    -- Mage
-    [62]  = { profileKey = "JetUI - Arcane Mage",    profileString = "PASTE_ARCANE_MAGE_PROFILE_STRING_HERE" },
-    [63]  = { profileKey = "JetUI - Fire Mage",      profileString = "PASTE_FIRE_MAGE_PROFILE_STRING_HERE" },
-    [64]  = { profileKey = "JetUI - Frost Mage",     profileString = "PASTE_FROST_MAGE_PROFILE_STRING_HERE" },
-    -- Monk
-    [268] = { profileKey = "JetUI - Brewmaster Monk",  profileString = "PASTE_BREWMASTER_MONK_PROFILE_STRING_HERE" },
-    [269] = { profileKey = "JetUI - Windwalker Monk",  profileString = "PASTE_WINDWALKER_MONK_PROFILE_STRING_HERE" },
-    [270] = { profileKey = "JetUI - Mistweaver Monk",  profileString = "PASTE_MISTWEAVER_MONK_PROFILE_STRING_HERE" },
-    -- Paladin
-    [65]  = { profileKey = "JetUI - Holy Paladin",   profileString = "PASTE_HOLY_PALADIN_PROFILE_STRING_HERE" },
-    [66]  = { profileKey = "JetUI - Prot Paladin",   profileString = "PASTE_PROT_PALADIN_PROFILE_STRING_HERE" },
-    [70]  = { profileKey = "JetUI - Ret Paladin",    profileString = "PASTE_RET_PALADIN_PROFILE_STRING_HERE" },
-    -- Priest
-    [256] = { profileKey = "JetUI - Disc Priest",    profileString = "PASTE_DISC_PRIEST_PROFILE_STRING_HERE" },
-    [257] = { profileKey = "JetUI - Holy Priest",    profileString = "PASTE_HOLY_PRIEST_PROFILE_STRING_HERE" },
-    [258] = { profileKey = "JetUI - Shadow Priest",  profileString = "PASTE_SHADOW_PRIEST_PROFILE_STRING_HERE" },
-    -- Rogue
-    [259] = { profileKey = "JetUI - Assassination Rogue", profileString = "PASTE_ASSASSINATION_ROGUE_PROFILE_STRING_HERE" },
-    [260] = { profileKey = "JetUI - Outlaw Rogue",        profileString = "PASTE_OUTLAW_ROGUE_PROFILE_STRING_HERE" },
-    [261] = { profileKey = "JetUI - Subtlety Rogue",      profileString = "PASTE_SUBTLETY_ROGUE_PROFILE_STRING_HERE" },
-    -- Shaman
-    [262] = { profileKey = "JetUI - Elemental Shaman",    profileString = "PASTE_ELEMENTAL_SHAMAN_PROFILE_STRING_HERE" },
-    [263] = { profileKey = "JetUI - Enhancement Shaman",  profileString = "PASTE_ENHANCEMENT_SHAMAN_PROFILE_STRING_HERE" },
-    [264] = { profileKey = "JetUI - Resto Shaman",        profileString = "PASTE_RESTO_SHAMAN_PROFILE_STRING_HERE" },
-    -- Warlock
-    [265] = { profileKey = "JetUI - Affliction Warlock",  profileString = "PASTE_AFFLICTION_WARLOCK_PROFILE_STRING_HERE" },
-    [266] = { profileKey = "JetUI - Demonology Warlock",  profileString = "PASTE_DEMONOLOGY_WARLOCK_PROFILE_STRING_HERE" },
-    [267] = { profileKey = "JetUI - Destruction Warlock", profileString = "PASTE_DESTRUCTION_WARLOCK_PROFILE_STRING_HERE" },
     -- Warrior
-    [71]  = { profileKey = "JetUI - Arms Warrior",   profileString = "PASTE_ARMS_WARRIOR_PROFILE_STRING_HERE" },
-    [72]  = { profileKey = "JetUI - Fury Warrior",   profileString = "PASTE_FURY_WARRIOR_PROFILE_STRING_HERE" },
-    [73]  = { profileKey = "JetUI - Prot Warrior",   profileString = "PASTE_PROT_WARRIOR_PROFILE_STRING_HERE" },
+    [11]  = { profileKey = "JetUI - Arms Warrior",        profileString = "PASTE_ARMS_WARRIOR_PROFILE_STRING_HERE" },
+    [12]  = { profileKey = "JetUI - Fury Warrior",        profileString = "PASTE_FURY_WARRIOR_PROFILE_STRING_HERE" },
+    [13]  = { profileKey = "JetUI - Prot Warrior",        profileString = "PASTE_PROT_WARRIOR_PROFILE_STRING_HERE" },
+    -- Paladin
+    [21]  = { profileKey = "JetUI - Holy Paladin",        profileString = "PASTE_HOLY_PALADIN_PROFILE_STRING_HERE" },
+    [22]  = { profileKey = "JetUI - Prot Paladin",        profileString = "PASTE_PROT_PALADIN_PROFILE_STRING_HERE" },
+    [23]  = { profileKey = "JetUI - Ret Paladin",         profileString = "PASTE_RET_PALADIN_PROFILE_STRING_HERE" },
+    -- Hunter
+    [31]  = { profileKey = "JetUI - BM Hunter",           profileString = "PASTE_BM_HUNTER_PROFILE_STRING_HERE" },
+    [32]  = { profileKey = "JetUI - MM Hunter",           profileString = "PASTE_MM_HUNTER_PROFILE_STRING_HERE" },
+    [33]  = { profileKey = "JetUI - SV Hunter",           profileString = "PASTE_SV_HUNTER_PROFILE_STRING_HERE" },
+    -- Rogue
+    [41]  = { profileKey = "JetUI - Assassination Rogue", profileString = "PASTE_ASSASSINATION_ROGUE_PROFILE_STRING_HERE" },
+    [42]  = { profileKey = "JetUI - Outlaw Rogue",        profileString = "PASTE_OUTLAW_ROGUE_PROFILE_STRING_HERE" },
+    [43]  = { profileKey = "JetUI - Subtlety Rogue",      profileString = "PASTE_SUBTLETY_ROGUE_PROFILE_STRING_HERE" },
+    -- Priest
+    [51]  = { profileKey = "JetUI - Disc Priest",         profileString = "PASTE_DISC_PRIEST_PROFILE_STRING_HERE" },
+    [52]  = { profileKey = "JetUI - Holy Priest",         profileString = "PASTE_HOLY_PRIEST_PROFILE_STRING_HERE" },
+    [53]  = { profileKey = "JetUI - Shadow Priest",       profileString = "PASTE_SHADOW_PRIEST_PROFILE_STRING_HERE" },
+    -- Death Knight
+    [61]  = { profileKey = "JetUI - Blood DK",            profileString = "PASTE_BLOOD_DK_PROFILE_STRING_HERE" },
+    [62]  = { profileKey = "JetUI - Frost DK",            profileString = "PASTE_FROST_DK_PROFILE_STRING_HERE" },
+    [63]  = { profileKey = "JetUI - Unholy DK",           profileString = "PASTE_UNHOLY_DK_PROFILE_STRING_HERE" },
+    -- Shaman
+    [71]  = { profileKey = "JetUI - Elemental Shaman",    profileString = "PASTE_ELEMENTAL_SHAMAN_PROFILE_STRING_HERE" },
+    [72]  = { profileKey = "JetUI - Enhancement Shaman",  profileString = "PASTE_ENHANCEMENT_SHAMAN_PROFILE_STRING_HERE" },
+    [73]  = { profileKey = "JetUI - Resto Shaman",        profileString = "PASTE_RESTO_SHAMAN_PROFILE_STRING_HERE" },
+    -- Mage
+    [81]  = { profileKey = "JetUI - Arcane Mage",         profileString = "PASTE_ARCANE_MAGE_PROFILE_STRING_HERE" },
+    [82]  = { profileKey = "JetUI - Fire Mage",           profileString = "PASTE_FIRE_MAGE_PROFILE_STRING_HERE" },
+    [83]  = { profileKey = "JetUI - Frost Mage",          profileString = "PASTE_FROST_MAGE_PROFILE_STRING_HERE" },
+    -- Warlock
+    [91]  = { profileKey = "JetUI - Affliction Warlock",  profileString = "PASTE_AFFLICTION_WARLOCK_PROFILE_STRING_HERE" },
+    [92]  = { profileKey = "JetUI - Demonology Warlock",  profileString = "PASTE_DEMONOLOGY_WARLOCK_PROFILE_STRING_HERE" },
+    [93]  = { profileKey = "JetUI - Destruction Warlock", profileString = "PASTE_DESTRUCTION_WARLOCK_PROFILE_STRING_HERE" },
+    -- Monk
+    [101] = { profileKey = "JetUI - Brewmaster Monk",     profileString = "PASTE_BREWMASTER_MONK_PROFILE_STRING_HERE" },
+    [102] = { profileKey = "JetUI - Mistweaver Monk",     profileString = "PASTE_MISTWEAVER_MONK_PROFILE_STRING_HERE" },
+    [103] = { profileKey = "JetUI - Windwalker Monk",     profileString = "PASTE_WINDWALKER_MONK_PROFILE_STRING_HERE" },
+    -- Druid
+    [111] = { profileKey = "JetUI - Balance Druid",       profileString = "PASTE_BALANCE_DRUID_PROFILE_STRING_HERE" },
+    [112] = { profileKey = "JetUI - Feral Druid",         profileString = "PASTE_FERAL_DRUID_PROFILE_STRING_HERE" },
+    [113] = { profileKey = "JetUI - Guardian Druid",      profileString = "PASTE_GUARDIAN_DRUID_PROFILE_STRING_HERE" },
+    [114] = { profileKey = "JetUI - Resto Druid",         profileString = "PASTE_RESTO_DRUID_PROFILE_STRING_HERE" },
+    -- Demon Hunter
+    [121] = { profileKey = "JetUI - Havoc DH",            profileString = "PASTE_HAVOC_DH_PROFILE_STRING_HERE" },
+    [122] = { profileKey = "JetUI - Vengeance DH",        profileString = "PASTE_VENGEANCE_DH_PROFILE_STRING_HERE" },
+    [123] = { profileKey = "JetUI - Aldrachi DH",         profileString = "PASTE_ALDRACHI_DH_PROFILE_STRING_HERE" },
+    -- Evoker
+    [131] = { profileKey = "JetUI - Devastation Evoker",  profileString = "PASTE_DEVASTATION_EVOKER_PROFILE_STRING_HERE" },
+    [132] = { profileKey = "JetUI - Preservation Evoker", profileString = "PASTE_PRESERVATION_EVOKER_PROFILE_STRING_HERE" },
+    [133] = { profileKey = "JetUI - Augmentation Evoker", profileString = "PASTE_AUGMENTATION_EVOKER_PROFILE_STRING_HERE" },
 }
