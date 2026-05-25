86ccbc33e74d891143b677be8f9582393b2d7249
Emil Thaudal Bønnerup <emil@thaudal.com>
2026-05-25 14:13:59 +0200

    AyijeCDM: add JetUI Caster profile for caster specs + Havoc DH
    - AddonData/AyijeCDM.lua: add placeholder JetUI Caster profile string
    - AddonImports/AyijeCDM.lua: add CASTER_SPECS table (mages, balance druid,
      shadow priest, elemental shaman, warlocks, BM/MM hunter, havoc DH)
      Profile selection priority: Healer > Caster > DPS

--- AddonImports/AyijeCDM.lua
+++ AddonImports/AyijeCDM.lua
@@ -1,4 +1,18 @@
 local HEALER_SPECS = { [65]=true, [256]=true, [257]=true, [105]=true, [264]=true, [270]=true, [1468]=true }
+local CASTER_SPECS = {
+    [62]=true,  -- Arcane Mage
+    [63]=true,  -- Fire Mage
+    [64]=true,  -- Frost Mage
+    [102]=true, -- Balance Druid
+    [258]=true, -- Shadow Priest
+    [262]=true, -- Elemental Shaman
+    [265]=true, -- Affliction Warlock
+    [266]=true, -- Demonology Warlock
+    [267]=true, -- Destruction Warlock
+    [253]=true, -- Beast Mastery Hunter
+    [254]=true, -- Marksmanship Hunter
+    [577]=true, -- Havoc Demon Hunter
+}
 
 function JetUI:ImportAyijeCDM(forceImport)
     if not C_AddOns.IsAddOnLoaded("Ayije_CDM") then return end
@@ -12,9 +26,16 @@ function JetUI:ImportAyijeCDM(forceImport)
         JetUIDB.InstalledVersions["AyijeCDM"] = C_AddOns.GetAddOnMetadata("JetUI", "X-AyijeCDM")
     end
 
-    -- Set active profile: DPS by default, healer if current spec is a healer
+    -- Set active profile: Caster > Healer > DPS
     local _, _, _, specID = GetSpecializationInfo(GetSpecialization() or 1)
-    local baseName = HEALER_SPECS[specID] and "JetUI Healer" or "JetUI DPS"
+    local baseName
+    if HEALER_SPECS[specID] then
+        baseName = "JetUI Healer"
+    elseif CASTER_SPECS[specID] then
+        baseName = "JetUI Caster"
+    else
+        baseName = "JetUI DPS"
+    end
     if Ayije_CDM_API.SetProfile then
         Ayije_CDM_API:SetProfile(prefix .. baseName)
     end
